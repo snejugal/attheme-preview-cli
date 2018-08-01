f5888cdc3579763f0ee4e44a83aca958374382b2
SnejUgal <snejugal@icloud.com>
2018-08-01 22:03:52 +0700

    Create arguments parser

---
+++ src/args.ts
@@ -0,0 +1,26 @@
+import { ArgumentParser } from "argparse";
+import * as path from "path";
+
+interface Arguments {
+  themePath: string;
+  outputPath: string;
+  templatePath: string;
+}
+
+const parser = new ArgumentParser({
+  addHelp: true,
+});
+
+parser.addArgument(`themePath`);
+parser.addArgument(`outputPath`);
+parser.addArgument([`-t`, `--template`], {
+  defaultValue: path.join(__dirname, `resources/template.svg`),
+  dest: `templatePath`,
+});
+
+const args: Arguments = parser.parseArgs();
+
+export default args;
+export {
+  Arguments,
+};
